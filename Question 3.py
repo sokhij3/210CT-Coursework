e9ab200b47f62820824cac41a92e289aac0700ba
sokhij3 <sokhij3@uni.coventry.ac.uk>
2016-12-02 16:38:27 +0000

    Question 3 Code

---
+++ Question 3.py
@@ -0,0 +1,12 @@
+#Q - Write the pseudocode for a function which returns the highest perfect square which is less
+#    or equal to its parameter (a positive integer). Implement this in a programming language of
+#    your choice.
+
+x = int(input("input an integer: "))
+
+def perfectSq(x):
+    y = x**0.5      #Square rooting the given integer.
+    p = int(y)**2   #squaring the integer of the sqrt will
+    print(p)        #give you the highest perfect square
+
+perfectSq(x)
